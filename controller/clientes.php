cf9879c447327f1fddfebb1b63f841474b01608f
zeta <jersson.z032@gmail.com>
2025-05-18 21:59:43 -0500

    FEAT(clientes): agrego modulo clientes, con crud funcionando

---
+++ controller/clientes.php
@@ -0,0 +1,82 @@
+<?php  
+class Clientes extends Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function get(){
+        $error = true;
+        $res = false;
+        $data = array();
+        if($result = $this->model->get()){
+            $res = true;
+        }
+        try{
+            while($row = mysqli_fetch_array($result)){
+                $data[] = array(
+                    "idusuario" => $row['idusuario'],
+                    "nombres" => $row['nombres'],
+                    "apellidos" => $row['apellidos'],
+                    "dni" => $row['dni'],
+                    "ciudad" => $row['ciudad'],
+                    "genero" => $row['genero'],
+                    "email" => $row['email'],
+                    "fechaNac" => $row['fechaNac'],
+                    "nivel" => $row['nivel'],
+                    "puntaje" => $row['puntaje'],
+                    "nivelLogin" => $row['level']
+                );
+            }
+            $error = false;
+        }catch(Exception $e){
+            $data['error'] = $e->getMessage();
+        }
+
+        $response = array(
+            "success" => $res,
+            "error" => $error,
+            "data" => $data
+        );
+        echo json_encode($response);
+    }
+    public function getById(){
+        $error = true;
+        $res = false;
+        $id = $_POST['idusuario'];
+        if($data = $this->model->getById($id)){
+            $res = true;
+            $error = false;
+        }
+        $response = array(
+            "success" => $res,
+            "error" => $error,
+            "data" => $data
+        );
+        echo json_encode($response);
+    }
+    public function update(){
+        $error = true;
+        $res = false;
+        $id = $_POST['idusuario'];
+        $nivel = $_POST['nivel'];
+        $puntaje = $_POST['puntaje'];
+        if($data = $this->model->update($id,$nivel,$puntaje)){
+            $res = true;
+            $error = false;
+        }
+        $response = array(
+            "success" => $res,
+            "error" => $error,
+            "data" => $data
+        );
+        echo json_encode($response);
+    }
+    public function delete(){
+
+    }
+}
+
+
+
+
+
+?>
---
+++ models/clientesmodel.php
@@ -0,0 +1,29 @@
+<?php 
+class ClientesModel extends Model{
+
+    function __construct(){
+        parent::__construct();
+    }
+    public function get(){
+        $sql = "SELECT u.*, l.nivel as level, l.estado FROM usuarios u JOIN login l ON u.idusuario = l.idusuario;";
+        $data = $this->conn->ConsultaCon($sql);
+        return $data;
+    }
+    public function getById($id){
+        $sql = "SELECT u.*, l.nivel as level, l.estado FROM usuarios u JOIN login l ON u.idusuario = l.idusuario WHERE u.idusuario = $id;";
+        $data = $this->conn->ConsultaArray($sql);
+        return $data;
+    }
+    public function update($id, $nivel, $puntaje){
+        $sql = "UPDATE usuarios SET nivel='$nivel', puntaje='$puntaje' WHERE idusuario = '$id';";
+        $result = $this->conn->ConsultaSin($sql);
+        return $result;
+    }
+    public function delete(){
+        $sql = "DELETE";
+        $result = $this->conn->ConsultaSin($sql);
+        return $result;
+    }
+}
+
+?>
